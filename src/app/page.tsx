86171efcc7fd7152a53add1760c30f39c3b68c9a
Hamdi Yahyaoui <hamdi_yahyaoui@yahoo.fr>
2024-07-28 20:55:25 +0200

    create first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,118 @@
-import Image from "next/image";
+import Head from 'next/head';
+import Image from 'next/image';
 
-export default function Home() {
+
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Head>
+        <title>Portfolio - Hamdi YAHYAOUI</title>
+        <meta name="description" content="Portfolio de Hamdi YAHYAOUI, Chargé de Communication." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="bg-white shadow-lg rounded-lg max-w-4xl w-full mx-4 p-6">
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/3 bg-blue-900 text-white p-4 rounded-lg md:rounded-r-none">
+            <Image src="/hamdi.jpg" alt="Profile Picture" className="rounded-full mx-auto" width={128} height={128} />
+            <div className="text-center mt-4">
+              <h1 className="text-2xl font-bold"></h1>
+              
+              <p className="text-sm">Autonome, Sens d’adaptation et du résultat, Déterminé, Travail en équipe</p>
+              <p className='text-sm'>Trilingue : Français, Anglais, Arabe </p> 
+            </div>
+            <div className="mt-6">
+              <h2 className="text-xl font-semibold">Informations</h2>
+              <ul className="text-sm mt-2">
+                <li>07.45.74.58.84</li>
+                <li>hamdi_yahyaoui@yahoo.fr</li>
+                <li>1 rue bagno à ripoli Le Plessis Robinson</li>
+                <li><a href="https://www.linkedin.com/in/hamdi-yahyaoui-230ba153">https://www.linkedin.com/in/hamdi-yahyaoui-230ba153</a></li>
+              </ul>
+            </div>
+            <div className="mt-6">
+              <h2 className="text-xl font-semibold">Compétences</h2>
+              <ul className="text-sm mt-2">
+                <li>Expertise dans le développement frontend avec ReactJS</li>
+                <li>Conception, implémentation et gestion d'APIs RESTful et GraphQL</li>
+                <li>Optimisation des Performances et Référencement (SEO)</li>
+                <li>Mise en place de tests unitaires, d'intégration et end-to-end</li>
+                <li>Collaboration et Animation des Workshops</li>
+              </ul>
+            </div>
+            
+          </div>
+          <div className="md:w-2/3 bg-white p-4 rounded-lg md:rounded-l-none">
+            <div className="mb-6">
+            <h2 className="text-2xl font-semibold text-blue-900">Hamdi YAHYAOUI</h2>
+            <p className="text-sm font-semibold text-blue-900">8 ans d'expérience, Développeur Full stack NodeJs/React</p>
+              <p className="text-xs mt-2 font-sans">Développeur Full Stack avec 8 ans d'expérience dans la conception et la mise en œuvre de solutions web innovantes. 
+                Expert en JavaScript et technologies associées, spécialisé dans le développement d'applications robustes et performantes. 
+                Proactif et orienté résultats, je m'adapte rapidement aux nouvelles technologies et méthodologies, 
+                et je m'épanouis dans des environnements collaboratifs et dynamiques.</p>
+
+              <h2 className="text-2xl font-semibold bg-blue-900 text-white mt-4">Compétences fonctionnelle</h2>
+              <div className='text-xs font-sans'>
+                <p >
+                ● Création des interfaces attrayantes, intuitives et réactives</p>
+                <p > ● Expertise dans la création de SPA pour offrir une expérience utilisateur fluide sans rechargement de page.</p>
+                <p >● Connaissance des principes de design UX pour améliorer l'expérience utilisateur</p>
+                <p >● Création d'APIs RESTful et GraphQL robustes et sécurisées.</p>
+                <p >● Gestion des authentifications, autorisations et contrôle d'accès.</p>
+                <p >● Connaissance approfondie des bases de données relationnelles et NoSQL</p>
+                <p >● Compétence en ORM (Object-Relational Mapping) avec des outils comme Prisma, Sequelize, Doctrine.</p>
+                <p >● Mise en place de pipelines CI/CD pour automatiser les tests et les déploiements</p>
+                <p >● Connaissance des pratiques DevOps pour l'intégration et le déploiement continus.</p>
+                <p >● Utilisation de Docker pour la création et la gestion de conteneurs.</p>
+                <p >● Maîtrise des méthodologies Agile (Scrum, Kanban)</p>
+                <p >● Capacité à animer des workshops, à faire des présentations techniques, et à former des équipes.</p>
+                <p >● Expertise dans les tests unitaires, d'intégration et end-to-end</p>
+                <p >● Connaissance des pratiques de refactoring pour maintenir un code propre et maintenable.</p>
+                <p >● Connaissance des mécanismes d'authentification et d'autorisation sécurisés (OAuth, JWT)</p>
+                <p >● Capacité à analyser et à résoudre rapidement les problèmes complexes rencontrés lors du développement.</p>
+               
+              </div>
+              <h2 className="text-2xl font-semibold bg-blue-900 text-white mt-4">Compétences technique</h2>
+              <div>
+                <p className='text-xs'>
+                Back-end et Front-end : Nodejs (nestjs, nextjs,expressjs), Reactjs, Nextjs
+BASES DE DONNÉES : MySQL,PostgresQL, MongoDB, firebase
+DEVOPS : Docker, CI/CD, Gitlab, GIT, Bitbucket
+Test : Cypress, Tests unitaires et d'intégration
+Méthodologies : Agile (Scrum)
+Documentation : Confluence, Notion
+Gestion de projet : Jira, Azure DevOps
+                </p>
+              </div>
+              <h2 className="text-2xl font-semibold bg-blue-900 text-white mt-4">Expériences Professionnelles</h2>
+              <div className="mt-4">
+                <h3 className="text-xl font-semibold">Ingénieur d'étude</h3>
+                <p className="text-sm text-gray-600">OGF : Juin 2023 - février 2024</p>
+                <p className="text-sm mt-2">Refonte du site web pfg.fr, réduisant les temps de chargement de 30%. 
+                Conception et développement d’une application configurateur marbrerie innovant pour les conseillers, augmentant les ventes de 15%.</p>
+              </div>
+              <div className="mt-4">
+                <h3 className="text-xl font-semibold">Ingénieur d'étude</h3>
+                <p className="text-sm text-gray-600">Symolia Technologie :  Novembre 2022 - Avril 2023</p>
+                <p className="text-sm mt-2">Piloter la migration des applications vers Docker, réduisant les coûts d'infrastructure de 25%.</p>
+              </div>
+            </div>
+            <div className="mb-6">
+            <h2 className="text-2xl font-semibold bg-blue-900 text-white mt-4">Formations</h2>
+              <div className="mt-4">
+                <h3 className="text-xl font-semibold">Master Informatique de Gestion- Spécialité Commerce Électronique </h3>
+                <p className="text-sm text-gray-600">2011 - 2013, Université de Jendouba - Tunis</p>
+              </div>
+              <div className="mt-4">
+                <h3 className="text-xl font-semibold">Licence fondamentale en informatique de gestion </h3>
+                <p className="text-sm text-gray-600">2007 - 2010, Université de Jendouba - Tunis</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Home;
